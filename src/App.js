027c530a5037843755f09ef8125d09be835eac73
Stefano <stefanopartipilo95@gmail.com>
2022-11-30 13:03:26 +0100

    Primo task

--- src/App.js
+++ src/App.js
@@ -4,10 +4,55 @@ import Table from "./components/Table/Table";
 
 function App() {
   const [data, setData] = useState(dataJson);
+  const [changeTable, setChangeTable] = useState(false);
+
+  const selectGroup = (value) => {
+    switch (value) {
+      case "1":
+        setData(dataJson);
+        setChangeTable(false);
+        break;
+      case "2":
+        groupingProject();
+        break;
+      default:
+        break;
+    }
+  };
+
+  const groupingProject = () => {
+    let arr = [];
+    for (let i = 0; i < data.length; i++) {
+      if (arr.length === 0) {
+        arr.push({
+          name: data[i].project.name,
+          hours: data[i].hours,
+        });
+      } else {
+        const findObj = arr.find((el) => el.name === data[i].project.name);
+        if (findObj?.name === data[i].project.name) {
+          let index = arr.findIndex((el) => el.name === data[i].project.name);
+          arr[index].hours += data[i].hours;
+        } else {
+          arr.push({
+            name: data[i].project.name,
+            hours: data[i].hours,
+          });
+        }
+      }
+    }
+    setChangeTable(true);
+    setData(arr);
+  };
+
   return (
     <>
       <div>
-        <select className="form-select w-25 m-4 shadow-sm" defaultValue={"1"}>
+        <select
+          className="form-select w-25 m-4 shadow-sm"
+          defaultValue={"1"}
+          onChange={(event) => selectGroup(event.target.value)}
+        >
           <option value="1">Senza aggregazioni</option>
           <option value="2">Raggruppamento per progetto</option>
           <option value="3">
@@ -19,7 +64,7 @@ function App() {
         </select>
       </div>
       <div className="mx-4 shadow-sm border border-dark">
-        <Table data={data} />
+        <Table data={data} changeTable={changeTable} />
       </div>
     </>
   );
--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,24 +1,34 @@
 import moment from "moment/moment";
 import React from "react";
 
-function Table({ data }) {
+function Table({ data, changeTable }) {
   return (
     <table className="table">
       <thead>
         <tr>
           <th scope="col">Project</th>
-          <th scope="col">Employee</th>
-          <th scope="col">Date</th>
+          {changeTable !== true && (
+            <>
+              <th scope="col">Employee</th>
+              <th scope="col">Date</th>
+            </>
+          )}
+
           <th scope="col">Hours</th>
         </tr>
       </thead>
       <tbody>
         {data.map((el, index) => (
           <tr key={index}>
-            <td>{el.project.name}</td>
-            <td>{el.employee.name}</td>
-            <td>{moment(el.date).format("ll")}</td>
-            <td>{el.hours}</td>
+            <td>{el.project?.name || el?.name}</td>
+            {changeTable !== true && (
+              <>
+                <td>{el.employee?.name}</td>
+                <td>{moment(el?.date).format("ll")}</td>
+              </>
+            )}
+
+            <td>{el?.hours}</td>
           </tr>
         ))}
       </tbody>

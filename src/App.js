7ee44dc7afa1f4b93420616269899a69ca764b53
Stefano <stefanopartipilo95@gmail.com>
2022-11-30 16:18:08 +0100

    Aggiornamento

--- src/App.js
+++ src/App.js
@@ -4,16 +4,23 @@ import Table from "./components/Table/Table";
 
 function App() {
   const [data, setData] = useState(dataJson);
-  const [changeTable, setChangeTable] = useState(false);
+  const [changeTable, setChangeTable] = useState(1);
 
   const selectGroup = (value) => {
     switch (value) {
       case "1":
         setData(dataJson);
-        setChangeTable(false);
+        setChangeTable(1);
         break;
       case "2":
         groupingProject();
+        setChangeTable(2);
+        break;
+      case "3":
+        groupingProjectAndEmployee();
+        setChangeTable(3);
+        break;
+      case "4":
         break;
       default:
         break;
@@ -22,26 +29,72 @@ function App() {
 
   const groupingProject = () => {
     let arr = [];
-    for (let i = 0; i < data.length; i++) {
+    for (let i = 0; i < dataJson.length; i++) {
+      // Se arr ha la lunghezza 0 (ovvero non ha nessun elemento all'interno), allora Il primo oggetto lo pushi in arr
       if (arr.length === 0) {
         arr.push({
-          name: data[i].project.name,
-          hours: data[i].hours,
+          name: dataJson[i].project.name,
+          hours: dataJson[i].hours,
         });
       } else {
-        const findObj = arr.find((el) => el.name === data[i].project.name);
-        if (findObj?.name === data[i].project.name) {
-          let index = arr.findIndex((el) => el.name === data[i].project.name);
-          arr[index].hours += data[i].hours;
+        // Altrimenti cercami il valore in arr che è uguale al valore dell'elemento in questo momento nel for
+        const findObj = arr.find((el) => el.name === dataJson[i].project.name);
+        // Se il nome del progetto trovato con il find è uguale al nome del progetto in dataJson
+        if (findObj?.name === dataJson[i].project.name) {
+          // Trovami l'index dell'oggetto
+          let index = arr.findIndex(
+            (el) => el.name === dataJson[i].project.name
+          );
+          // Addiziona le ore precedenti con quelle nuove
+          arr[index].hours += dataJson[i].hours;
         } else {
+          // Altrimenti pushami il nuovo oggetto (ovvero il nuovo progetto che attualmente non c'è nell'array "arr")
           arr.push({
-            name: data[i].project.name,
-            hours: data[i].hours,
+            name: dataJson[i].project.name,
+            hours: dataJson[i].hours,
           });
         }
       }
     }
-    setChangeTable(true);
+    // Inserisci l'array nello state data
+    setData(arr);
+  };
+
+  const groupingProjectAndEmployee = () => {
+    let arr = [];
+    for (let i = 0; i < dataJson.length; i++) {
+      if (arr.length === 0) {
+        arr.push({
+          name: dataJson[i].project.name,
+          hours: dataJson[i].hours,
+          employee: dataJson[i].employee.name,
+          date: dataJson[i].date,
+        });
+      } else {
+        const findObj = arr.find((el) => el.name === dataJson[i].project.name);
+        if (
+          findObj?.name === dataJson[i].project.name &&
+          findObj?.employee === dataJson[i].employee.name
+        ) {
+          let index = arr.findIndex(
+            (el) => el.name === dataJson[i].project.name
+          );
+          arr[index].hours += dataJson[i].hours;
+        } else {
+          arr.push({
+            name: dataJson[i].project.name,
+            hours: dataJson[i].hours,
+            employee: dataJson[i].employee.name,
+            date: dataJson[i].date,
+          });
+        }
+      }
+    }
+    // Ordine alfabetico - nome del progetto (trovato sul web)
+    arr.sort((a, b) => {
+      if (a.name > b.name) return -1;
+      else return 1;
+    });
     setData(arr);
   };
 
--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -7,10 +7,17 @@ function Table({ data, changeTable }) {
       <thead>
         <tr>
           <th scope="col">Project</th>
-          {changeTable !== true && (
+          {changeTable !== 2 && (
             <>
               <th scope="col">Employee</th>
-              <th scope="col">Date</th>
+              <th
+                scope="col"
+                style={
+                  changeTable === 3 ? { display: "none" } : { display: "block" }
+                }
+              >
+                Date
+              </th>
             </>
           )}
 
@@ -21,13 +28,20 @@ function Table({ data, changeTable }) {
         {data.map((el, index) => (
           <tr key={index}>
             <td>{el.project?.name || el?.name}</td>
-            {changeTable !== true && (
+            {changeTable !== 2 && (
               <>
-                <td>{el.employee?.name}</td>
-                <td>{moment(el?.date).format("ll")}</td>
+                <td>{el.employee?.name || el?.employee}</td>
+                <td
+                  style={
+                    changeTable === 3
+                      ? { display: "none" }
+                      : { display: "block" }
+                  }
+                >
+                  {moment(el?.date).format("LL", "it-IT")}
+                </td>
               </>
             )}
-
             <td>{el?.hours}</td>
           </tr>
         ))}
